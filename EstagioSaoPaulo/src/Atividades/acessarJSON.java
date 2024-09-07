99cd5a2e99243a216ba07ebd48ac8ca9e1d31fcb
GabrielLChagas <Chagasg280@outlook.com>
2024-09-06 23:43:54 -0300

    Atualização3

---
+++ EstagioSaoPaulo/src/Atividades/acessarJSON.java
@@ -0,0 +1,31 @@
+package Atividades;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class acessarJSON {
+
+    public static String getJSON(String strUrl) {
+        String textoJson = "";
+
+        try {
+            URL url = new URL(strUrl);
+            InputStream is = url.openStream();
+
+            BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                textoJson += line + "\n";
+            }
+
+            is.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return textoJson;
+    }
+}
--- EstagioSaoPaulo/src/Atividades/atividade3.java
+++ EstagioSaoPaulo/src/Atividades/atividade3.java
@@ -1,4 +1,25 @@
 package Atividades;
 
-public class atividade3 {
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class atividade3 extends acessarJSON {
+
+    String dados = getJSON(
+            "https://raw.githubusercontent.com/GabrielLChagas/EstagioSaoPaulo/main/dadosDeDaturamento");
+
+    try {
+        JSONParser dados = new JSONParser();
+        Object  objeto = dados.parse(dados);
+        JSONObject objetoJson = (JSONObject) objeto;
+
+        JSONObject listaDados = (JSONObject) objetoJson.get();
+    }
+    catch(Exception ex) {
+        ex.printStackTrace();
+    }
+
+    public void inprimir(){
+        System.out.println(dados);
+    }
 }
